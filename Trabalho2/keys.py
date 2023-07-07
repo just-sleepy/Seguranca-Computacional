b63618fb236bc3a055339acaf729ac72457e9f78
BrunoVarg <brunovargas7899@gmail.com>
2023-07-07 09:49:49 -0300

    fixing generate key

--- Trabalho2/keys.py
+++ Trabalho2/keys.py
@@ -1,11 +1,4 @@
 import random
-BLOCK_SIZE = 128
 
 def generate_key():
-    key = []
-    n = random.randint(0, 0xffffffffffffffffffffffffffffffff)
-    for i in range(int(BLOCK_SIZE/8)):
-        key.append(n&0xff)
-        n >>= 8
-
-    return key
+    return [random.randint(0, 0xff) for _ in range(16)]
